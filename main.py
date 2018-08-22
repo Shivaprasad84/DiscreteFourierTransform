05f33f67f0268f8dd358d949f860048c3798f95a
Shivaprasad S <35187269+Shivaprasad84@users.noreply.github.com>
2018-08-22 14:11:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import dsp
+
+Xn = [complex(x) for x in input('x(n): ').strip().split()]
+N = int(input('N: '))
+
+# Discrete fourier transform:
+Xk = dsp.dft(Xn, N)
+print('X(k): ', Xk)
+
+# Polar form of Xk:
+pol = dsp.polar(Xk)
+print('Polar Form: ', pol)
+
+# Inverse discrete fourier transform:
+Xn = dsp.idft(Xk, N)
+print(Xn)
